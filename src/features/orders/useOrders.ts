6356824b39466297f1de94e6d38c7a3739ec928a
dalissongabriel <alissongabrieldino@gmail.com>
2022-06-26 17:29:38 -0300

    refactor(orders): :art: improve useOrders zustand hook

--- src/features/orders/useOrders.ts
+++ src/features/orders/useOrders.ts
@@ -1,23 +1,27 @@
 import create from "zustand";
 
-import { fetchOrders } from "@/shared/services/OrdersAPI";
+import OrdersAPI from "@/shared/services/OrdersAPI";
 import { IOrder } from "@/features/orders/types";
 
-interface Store {
+interface OrdersState {
   orders: IOrder[];
   addOrder: (data: IOrder) => void;
   resetOrders: () => void;
   fetchOrders: () => void;
 }
 
-export const useOrders = create<Store>((set) => ({
+export const useOrders = create<OrdersState>((set) => ({
+  // initial state
   orders: [],
-  addOrder: (data) =>
+  // "reducers"
+  addOrder: (data: IOrder) =>
     set((state) => ({ ...state, orders: [...state.orders, data] })),
+
   resetOrders: () => set((state) => ({ ...state, orders: [] })),
+
   fetchOrders: async () => {
     try {
-      const response = await fetchOrders();
+      const response = await OrdersAPI.fetchOrders();
       set((state) => ({
         ...state,
         orders: [...state.orders, ...response.data],
--- src/shared/services/OrdersAPI.ts
+++ src/shared/services/OrdersAPI.ts
@@ -1,6 +1,12 @@
 import { httpClient } from "@/infra/HttpClient";
 import { IOrder } from "@/features/orders/types";
 
-export async function fetchOrders() {
+async function fetchOrders() {
   return httpClient.get<IOrder[]>("/orders");
 }
+
+const OrdersAPI = {
+  fetchOrders,
+};
+
+export default OrdersAPI;
